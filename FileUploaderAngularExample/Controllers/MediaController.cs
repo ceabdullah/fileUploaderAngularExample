1c45a00964c52a74e71e85313e1fb6805e0310de
abdullah.demirtas <ceabdullahdemirtas@gmail.com>
2019-09-09 15:51:02 +0300

    Add project files.

---
+++ FileUploaderAngularExample/Controllers/MediaController.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Net.Http.Headers;
+
+namespace FileUploaderAngularExample.Controllers
+{
+    public class MediaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private static readonly FormOptions _defaultFormOptions = new FormOptions();
+
+        [HttpPost]
+        public async Task<IActionResult> Upload()
+        {
+            string targetFilePath = null;
+
+            var boundary = MultipartRequestHelper.GetBoundary(MediaTypeHeaderValue.Parse(Request.ContentType), _defaultFormOptions.MultipartBoundaryLengthLimit);
+            var reader = new MultipartReader(boundary, HttpContext.Request.Body);
+
+            var section = await reader.ReadNextSectionAsync();
+
+            targetFilePath = Path.GetTempFileName();
+            using (FileStream targetStream = System.IO.File.Create(targetFilePath))
+            {
+                await section.Body.CopyToAsync(targetStream);
+            }
+
+            return Json(new
+            {
+
+            });
+        }
+    }
+}
